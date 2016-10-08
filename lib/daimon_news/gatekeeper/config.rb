f7243758a38fef1abb18325cf775f49c0d1b37ce
Atsushi Fukuda <atsushi-fukuda@bm-sms.co.jp>
2016-10-09 07:13:37 +0900

    make refactoring.

--- lib/daimon_news/gatekeeper/config.rb
+++ lib/daimon_news/gatekeeper/config.rb
@@ -16,12 +16,16 @@ module DaimonNews
       config_accessor :sign_in_url
       config_accessor :sign_in_status_url
       config_accessor :member_only_keyword
+      config_accessor :parameter_name_to_return
+      config_accessor :loading_message
     end
 
     configure do |config|
-      config.member_only_keyword = '(会員限定記事)'
-      config.sign_in_status_url = nil
+      config.member_only_keyword = '(members only)'
+      config.sign_in_status_url = 'https://example.com/'
       config.sign_in_url = nil
+      config.parameter_name_to_return = 'return_to'
+      config.loading_message = 'Now Loading..'
     end
 
   end
