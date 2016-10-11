56c8a896662eef4584b375d8c0566e6f4a435723
Atsushi Fukuda <atsushi-fukuda@bm-sms.co.jp>
2016-10-11 15:25:48 +0900

    for backup (Add bower configuration.)

--- lib/daimon_news/gatekeeper.rb
+++ lib/daimon_news/gatekeeper.rb
@@ -2,7 +2,6 @@ require 'daimon_news/gatekeeper/version'
 require 'daimon_news/gatekeeper/engine'
 require 'sprockets/es6'
 require 'jquery-rails'
-require 'rails-assets-css-spinners'
 require 'daimon_news/gatekeeper/config'
 
 module DaimonNews
